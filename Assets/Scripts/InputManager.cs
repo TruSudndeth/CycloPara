4cf0c19035ffd023dadfad304fc48267f9f3aec3
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2023-02-03 19:01:58 -0800

    Set up unity Input system

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private BaseCycloParaInputs _baseInputs;
+    private void Awake()
+    {
+        _baseInputs = new BaseCycloParaInputs();
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        
+    }
+}
