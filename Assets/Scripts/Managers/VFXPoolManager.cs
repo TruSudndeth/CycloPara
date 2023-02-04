da90d6f6f124da0201c110e3cd2e0d0a078bc75e
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2023-02-04 01:35:32 -0800

    Created prerequisites for VFXPoolManager

--- Assets/Scripts/Managers/VFXPoolManager.cs
+++ Assets/Scripts/Managers/VFXPoolManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class VFXPoolManager : MonoBehaviour
 {
+    
+    public static VFXPoolManager Instance { get; private set; }
+    [SerializeField]
+    private Transform[] gameObjectPool;
+    private List<Transform> vfxPool;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        vfxPool = new(gameObjectPool.Count() * 3);
     }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
