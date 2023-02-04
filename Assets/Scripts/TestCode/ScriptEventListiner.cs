313a58297d56d058396af3eacee08121bb07acc2
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2023-02-03 23:07:32 -0800

    Completed Audio Manager with test scripts

--- Assets/Scripts/TestCode/ScriptEventListiner.cs
+++ Assets/Scripts/TestCode/ScriptEventListiner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows;
+using SonicBloom.Koreo;
 
 public class ScriptEventListiner : MonoBehaviour
 {
@@ -10,12 +11,32 @@ public class ScriptEventListiner : MonoBehaviour
     [SerializeField]
     private float _resetTime = 3f;
     private bool _interacted = false;
+    private bool _alternate = false;
+    
+    public delegate void KoreographyEventCallback(KoreographyEvent koreoEvent);
 
     private MeshRenderer _meshRend;
     void Start()
     {
         _meshRend = GetComponent<MeshRenderer>();
-        InputManager.OnInteraction += SetTime;
+        //InputManager.OnInteraction += SetTime;
+        Koreographer.Instance.RegisterForEvents("Base_BPM-Drums", FireEventWDebg);
+    }
+    void FireEventWDebg(KoreographyEvent koreoE)
+    {
+        Debug.Log("Koreo Event fired");
+        if(_alternate)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            _meshRend.material.color = Color.red;
+            _alternate = false;
+        }
+        else
+        {
+            transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            _meshRend.material.color = Color.blue;
+            _alternate = true;
+        }
     }
     void Update()
     {
