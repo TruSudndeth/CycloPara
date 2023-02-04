90c7a61c729d4750dfe352521bdf975323a81b40
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2023-02-04 10:45:50 -0800

    input system Unity crash.

--- Assets/Scripts/Managers/VFXPoolManager.cs
+++ Assets/Scripts/Managers/VFXPoolManager.cs
@@ -8,25 +8,52 @@ public class VFXPoolManager : MonoBehaviour
     
     public static VFXPoolManager Instance { get; private set; }
     [SerializeField]
-    private Transform[] gameObjectPool;
+    private Transform[] _vfxTransforms;
+    [SerializeField]
     private List<Transform> vfxPool;
+    private bool _mouseClicked = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        vfxPool = new(gameObjectPool.Count() * 3);
+        vfxPool = new(_vfxTransforms.Count() * 3);
+        //do a lamda express for imputmanager interact mouse set _mouseClicked to true
+        PopulateVFXPool();
     }
-    // Update is called once per frame
+    private void SubscribeTeEvents()
+    {
+        InputManager.OnInteractionMouse += () => _mouseClicked = true;
+    }
+    private void PopulateVFXPool()
+    {
+        foreach (var vfx in _vfxTransforms)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var vfxInstance = Instantiate(vfx, transform);
+                vfxInstance.gameObject.SetActive(false);
+                vfxPool.Add(vfxInstance);
+            }
+        }
+    }
     void Update()
     {
+        
+        if (_mouseClicked)
+        {
+            _mouseClicked = false;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(InputManager.Instance.Look);
+            SpawnVFXFromPool(mousePos, VFXType.vfxDefault);
+        }
 
     }
     private void SpawnVFXFromPool(Vector3 location, VFXType vfxType)
     {
+        Debug.Log("mousedClicked");
         Transform vfx = vfxPool.Find(x => x.gameObject.activeSelf == false && x.GetComponent<VFXRoot>().VFXType == vfxType);
         if (vfx == null)
         {
-            vfx = Instantiate(gameObjectPool[(int)vfxType], location, Quaternion.identity);
+            vfx = Instantiate(_vfxTransforms[(int)vfxType], location, Quaternion.identity);
             vfxPool.Add(vfx);
         }
         else
