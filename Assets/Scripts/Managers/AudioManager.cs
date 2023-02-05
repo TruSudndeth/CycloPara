0914995a472d37367244fa2e383047c407c67b01
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2023-02-05 09:19:41 -0800

    Fix large files. Setup Audio manager for the Game manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -7,25 +7,39 @@ using System.Runtime.CompilerServices;
 
 public class AudioManager : MonoBehaviour
 {
+    //Use Call the instance and obtain a koreoStage track and return that to this isntance and play it.
+    //to call LoadKoreoTrack() must obtain Track using the instance and passing that to LoadKoreoTrack();
+    //null check outside this skript for missing tracks.
     public static AudioManager Instance;
 
     private SimpleMusicPlayer _koreoMusicPlayer;
+    private AudioSource _mainAudioSource;
     
     [SerializeField]
-    private Koreography _koreographer1;
+    private Koreography _koreoStage1;
+    public Koreography KoreoStage1 { get { return _koreoStage1; } private set { } }
     [SerializeField]
-    private Koreography _koreographer2;
-    private AudioSource _mainAudioSource;
+    private Koreography _koreoStage2;
+    public Koreography KoreoStage2 { get { return _koreoStage2; } private set { } }
+    
+    [SerializeField]
+    private Koreography _koreoStage3;
+    public Koreography KoreoStage3 { get { return _koreoStage3; } private set { } }
+    [SerializeField]
+    private Koreography _koreoStage4;
+    public Koreography KoreoStage4 { get { return _koreoStage4; } private set { } }
+    [SerializeField]
+    private Koreography _koreoStage5;
+    public Koreography KoreoStage5 { get { return _koreoStage5; } private set { } }
+
+    [Space]
+    [SerializeField]
+    private List<Transform> _sfxTracks;
+    
 
     [Space]
     [SerializeField]
     private AudioSource _audioSource;
-    [SerializeField]
-    private List<Transform> _musicTracks;
-    [SerializeField]
-    private List<Transform> _sfxTracks;
-    [SerializeField]
-    private List<Transform> _eventTracks;
 
     private void Start()
     {
@@ -35,18 +49,27 @@ public class AudioManager : MonoBehaviour
             Instance = this;
         else
             Destroy(gameObject);
-        _koreoMusicPlayer.LoadSong(_koreographer1, 0, false);
+        _koreoMusicPlayer.LoadSong(_koreoStage1, 0, false);
         _koreoMusicPlayer.Play();
         _mainAudioSource.loop = true;
         StartCoroutine(NextTrackEvent());
+    }
+    public void LoadKoreoTrack(Koreography koreoTrack)
+    {
+        _koreoMusicPlayer.LoadSong(koreoTrack, 0, false);
+        _mainAudioSource.loop = true;
+    }
+    public void PlayKoreoTrack()
+    {
+        _koreoMusicPlayer.Play();
     }
     IEnumerator NextTrackEvent()
     {
         //Get the audioSouces length from track
         //and wait for the length of the track
         float trackLength = _mainAudioSource.clip.length;
-        yield return new WaitForSeconds(trackLength * 0.5f);
-        _koreoMusicPlayer.LoadSong(_koreographer2, 0, false);
+        yield return new WaitForSeconds(3.0f);
+        _koreoMusicPlayer.LoadSong(_koreoStage2, 0, false);
         _koreoMusicPlayer.Play();
     }
     private void OnDisable()
